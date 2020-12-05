09720e48e0623f30ace51b4842a2d6e232f27877
Javier Fernández <jfernandz1290@gmail.com>
2020-12-06 00:16:45 +0100

    Fixes for Both-Alongside-5_4.sh (gen by arandr)

--- screenlayout/.screenlayout/Both-Alongside-5_4.sh
+++ screenlayout/.screenlayout/Both-Alongside-5_4.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output eDP1 --mode 1366x768 --pos 0x0 --rotate normal \
-	--output HDMI1 --primary --mode 1280x1024 --pos 1366x0 --rate 60.02
+xrandr --output VGA-0 --mode 1280x1024 --pos 1920x0 --rotate normal \
+	--output DVI-D-0 --off --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal
