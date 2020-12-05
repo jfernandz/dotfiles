d6e91042b9702a9e3adf5f9fb4d531afa4c1b6c1
Javier Fernández <jfernandz1290@gmail.com>
2020-12-06 00:14:06 +0100

    Updated both-alongside 5:4 screenlayout

--- screenlayout/.screenlayout/Both-Alongside-5_4.sh
+++ screenlayout/.screenlayout/Both-Alongside-5_4.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output eDP1 --primary --mode 1366x768 --pos 0x0 --rotate normal \
-	--output HDMI1 --mode 1280x1024 --pos 1366x0 --rate 60.02
+xrandr --output eDP1 --mode 1366x768 --pos 0x0 --rotate normal \
+	--output HDMI1 --primary --mode 1280x1024 --pos 1366x0 --rate 60.02
